ac74e07ea561087e5bf2bc7586d0a7d4bdb2fd85
Carlos Cruz <149225966+CarlosgCruz@users.noreply.github.com>
2026-01-11 11:12:29 -0400

    Add user input for concatenating first and last name

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,9 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+dado1 = input("Nome: ")
+dado2 = input("Sobrenome: ")
+
+# O Python substitui o que está entre { } pelo valor da variável
+resultado = f"{dado1} {dado2}"
+
+print(f"O nome completo é: {resultado}")
