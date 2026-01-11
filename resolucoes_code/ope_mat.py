f27247e32b7fe951061207348fe7a52047779353
Carlos Cruz <149225966+CarlosgCruz@users.noreply.github.com>
2026-01-11 11:14:20 -0400

    Add input and addition functionality to ope_mat.py

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,8 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+resultado = num1 + num2
+
+print(f"A soma entre {num1} e {num2} é igual a {resultado}")
